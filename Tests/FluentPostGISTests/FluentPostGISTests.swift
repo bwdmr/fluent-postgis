c4d6909ccf3f48b09ad6b7090b91b0e9a56f0c72
Nikolai Guyot <nikolai.guyot@code.berlin>
2022-05-03 00:53:01 +0200

    Clean up tests

--- Tests/FluentPostGISTests/FluentPostGISTests.swift
+++ Tests/FluentPostGISTests/FluentPostGISTests.swift
@@ -4,8 +4,6 @@ import PostgresKit
 import XCTest
 
 class FluentPostGISTests: XCTestCase {
-    var eventLoopGroup: EventLoopGroup!
-    var threadPool: NIOThreadPool!
     var dbs: Databases!
     var db: Database {
         self.dbs.database(
@@ -14,21 +12,16 @@ class FluentPostGISTests: XCTestCase {
         )!
     }
 
-    var postgres: PostgresDatabase {
-        self.db as! PostgresDatabase
-    }
-
     override func setUp() {
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let threadPool = NIOThreadPool(numberOfThreads: 1)
+        self.dbs = Databases(threadPool: threadPool, on: eventLoopGroup)
         let configuration = PostgresConfiguration(
             hostname: "localhost",
             username: "fluentpostgis",
             password: "fluentpostgis",
             database: "postgis_tests"
         )
-
-        self.eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        self.threadPool = NIOThreadPool(numberOfThreads: 1)
-        self.dbs = Databases(threadPool: self.threadPool, on: self.eventLoopGroup)
         self.dbs.use(.postgres(configuration: configuration), as: .psql)
     }
 }
--- Tests/LinuxMain.swift
+++
@@ -1,8 +0,0 @@
-// import XCTest
-//
-// import FluentPostGISTests
-//
-// var tests = [XCTestCaseEntry]()
-// tests += FluentPostGISTests.__allTests()
-//
-// XCTMain(tests)
